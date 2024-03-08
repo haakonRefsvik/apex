80a81ad7badbeacd525dae538677076c89601c17
Håkon Bjørgen Refsvik <haakoref@ulrik.uio.no>
2024-03-08 23:04:07 +0100

    endret apikey

--- app/src/main/java/no/uio/ifi/in2000/rakettoppskytning/data/API_KEY.kt
+++ app/src/main/java/no/uio/ifi/in2000/rakettoppskytning/data/API_KEY.kt
@@ -2,7 +2,7 @@ package no.uio.ifi.in2000.rakettoppskytning.data
 
 class API_KEY{
 
-    private val apiKey: String = "efeffad2-735a-43f7-9476-63b083165c15"
+    private val apiKey: String = ""
     fun getKey(): String{
         return apiKey
     }
