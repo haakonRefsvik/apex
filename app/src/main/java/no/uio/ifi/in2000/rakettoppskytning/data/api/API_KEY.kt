bf5af8bc02723100e61caf5bd4b373d0c6638031
Håkon Bjørgen Refsvik <haakoref@ulrik.uio.no>
2024-03-08 15:22:27 +0100

    oppdatert .gitignore

--- app/src/main/java/no/uio/ifi/in2000/rakettoppskytning/data/api/API_KEY.kt
+++
@@ -1,9 +0,0 @@
-package no.uio.ifi.in2000.rakettoppskytning.data.api
-
-class API_KEY{
-
-    private val apiKey: String = "df0249c5-183e-442e-833b-7d255d26521d"
-    fun getKey(): String{
-        return apiKey
-    }
-}
