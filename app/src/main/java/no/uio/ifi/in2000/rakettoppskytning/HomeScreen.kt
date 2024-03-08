fd2d05200ef09dbee3cafe3b0b05cbd24b4a9a87
Julian Rubiz <jarubila@uio.no>
2024-03-08 02:38:43 +0100

    Lag til feature \n Har lagt til et eksempel til favoritter men uten funksjonalitet

--- app/src/main/java/no/uio/ifi/in2000/rakettoppskytning/HomeScreen.kt
+++ app/src/main/java/no/uio/ifi/in2000/rakettoppskytning/HomeScreen.kt
@@ -7,6 +7,7 @@ import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,15 +16,20 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.Text
@@ -73,6 +79,7 @@ import kotlinx.coroutines.launch
 import java.time.Instant
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
+
 fun String.isDouble(): Boolean {
     return try {
         this.toDouble()
@@ -88,7 +95,7 @@ fun String.isDouble(): Boolean {
 fun HomeScreen(
     navController: NavHostController,
     homeScreenViewModel: HomeScreenViewModel = viewModel()
-    ) {
+) {
 
     val forecast by homeScreenViewModel.foreCastUiState.collectAsState()
     val lat by homeScreenViewModel.lat
@@ -97,6 +104,18 @@ fun HomeScreen(
 
     //val scaffoldState = rememberBottomSheetScaffoldState()
     val scaffoldState by homeScreenViewModel.bottomSheetScaffoldState
+    val favoritter = listOf<String>(
+        "Lokasjon1",
+        "Lokasjon2",
+        "Lokasjon3",
+        "Lokasjon4",
+        "Lokasjon5",
+        "Lokasjon6",
+        "Lokasjon7",
+        "Lokasjon8",
+        "Lokasjon9",
+        "Lokasjon10",
+    )
 
 
     /*** HUSKE Å LEGGE TIL UISATE SLIK AT TING BLIR HUSKET NÅR MAN NAVIGERER!!!***/
@@ -166,7 +185,6 @@ fun HomeScreen(
                 Text(text = "BottomAppBar")
 
 
-
             }
         }
     ) { innerPadding ->
@@ -176,131 +194,183 @@ fun HomeScreen(
                 .fillMaxSize()
         ) {
 
-            BottomSheetScaffold(scaffoldState  = scaffoldState, sheetPeekHeight = 160.dp, sheetContent ={
-                Box{
-                    Column(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.SpaceBetween,
+            BottomSheetScaffold(
+                scaffoldState = scaffoldState,
+                sheetPeekHeight = 156.dp,
+                sheetContent = {
+                    Box {
+                        Column(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.SpaceBetween,
 
 
-                        content =
-                        {
+                            content =
+                            {
 
 
-                            val currentInstant = Instant.now().plus(1,ChronoUnit.HOURS)
-                            val formatter = DateTimeFormatter.ISO_INSTANT
+                                val currentInstant = Instant.now().plus(1, ChronoUnit.HOURS)
+                                val formatter = DateTimeFormatter.ISO_INSTANT
 
 
-                            val formattedInstant = formatter.format(currentInstant)
+                                val formattedInstant = formatter.format(currentInstant)
 
-                            val newInstant = currentInstant.plus(6, ChronoUnit.HOURS)
+                                val newInstant = currentInstant.plus(6, ChronoUnit.HOURS)
 
-                            val formattedInstantAfter = formatter.format(newInstant)
-                            Row {
-                                TextField(value = lat.toString(), onValueChange ={value -> if(value.isDouble()){homeScreenViewModel.lat.value = value.toDouble().coerceIn(-90.0, 90.0)}
-                                }, Modifier.width(160.dp), textStyle = TextStyle(fontSize = 12.sp),
-                                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done, keyboardType = KeyboardType.Number),
-                                    keyboardActions = KeyboardActions(
-                                        onDone = {controller?.hide()}
-                                    ))
-                                Spacer(modifier = Modifier.width(20.dp))
-                                TextField(value = lon.toString(), onValueChange ={value -> if (value.isDouble()){homeScreenViewModel.lon.value = if(value.toDouble().isInfinite()) 0.0 else value.toDouble()}}, Modifier.width(160.dp) , textStyle = TextStyle(fontSize = 12.sp),
-                                    keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done, keyboardType = KeyboardType.Number),
-                                    keyboardActions = KeyboardActions(
-                                        onDone = {controller?.hide()}
-                                    )
-                                )
-
-                            }
-                            Spacer(modifier = Modifier.height(5.dp))
-                            Row {
-                                Button(modifier = Modifier.width(145.dp),onClick = {
-                                    homeScreenViewModel.getForecastByCord(lat,lon)
-                                    mapViewportState.flyTo(
-                                        cameraOptions = cameraOptions {
-                                            center(Point.fromLngLat(lon, lat))
-
-                                        },)
-                                    scope.launch{
-                                        scaffoldState.bottomSheetState.expand()} }) {
-                                    Text("Legg  favoritter")
-
-                                }
-                                Spacer(modifier = Modifier.width(35.dp))
-                                Button(modifier = Modifier.width(145.dp),onClick = {
-                                    homeScreenViewModel.getForecastByCord(lat,lon)
-                                    mapViewportState.flyTo(
-                                        cameraOptions = cameraOptions {
-                                            center(Point.fromLngLat(lon, lat))
-
-                                        },)
-                                    scope.launch{
-                                        scaffoldState.bottomSheetState.expand()} }) {
-                                    Text("Hent værdata")
-
-                                }
-
-                            }
-
-                            Spacer(modifier = Modifier.height(5.dp))
-                            LazyColumn(content = { item {
-                                forecast.foreCast.forEach breaking@{
-                                        input ->
-                                    input.properties.timeseries.forEach lit@{
-                                            tider->
-                                        if( tider.time < formattedInstant){
-                                            return@lit
-                                        }
-                                        if(formattedInstant < tider.time && tider.time < formattedInstantAfter){
-                                            val klokkeslett = tider.time.substring(11,16)
-                                            Spacer(modifier = Modifier.height(7.5.dp))
-                                            ElevatedCard(
-                                                modifier = Modifier
-                                                    .height(100.dp)
-                                                    .width(340.dp),
-                                                onClick ={
-                                                    val json = Uri.encode(Gson().toJson(tider.data.instant.details))
-                                                    navController.navigate("DetailsScreen/${json}")})
-                                            {
-
-
-
-                                                Text("Kl:$klokkeslett")
-                                                Text(" ${tider.data.instant.details.airTemperature}°C" )
-                                                tider.data.next1Hours?.summary?.let { Text(it.symbolCode) }
-
-
-
+                                val formattedInstantAfter = formatter.format(newInstant)
+                                Row {
+                                    TextField(
+                                        value = lat.toString(),
+                                        onValueChange = { value ->
+                                            if (value.isDouble()) {
+                                                homeScreenViewModel.lat.value =
+                                                    value.toDouble().coerceIn(-90.0, 90.0)
                                             }
+                                        },
+                                        Modifier
+                                            .width(160.dp)
+                                            .height(50.dp),
+                                        textStyle = TextStyle(fontSize = 12.sp),
+                                        keyboardOptions = KeyboardOptions(
+                                            imeAction = ImeAction.Done,
+                                            keyboardType = KeyboardType.Number
+                                        ),
+                                        keyboardActions = KeyboardActions(
+                                            onDone = { controller?.hide() }
+                                        ),
+                                        label = { Text("Longitude") },
+                                        singleLine = true,
+                                    )
+                                    Spacer(modifier = Modifier.width(20.dp))
+                                    TextField(value = lon.toString(),
+                                        onValueChange = { value ->
+                                            if (value.isDouble()) {
+                                                homeScreenViewModel.lon.value = if (value.toDouble()
+                                                        .isInfinite()
+                                                ) 0.0 else value.toDouble()
+                                            }
+                                        },
 
-                                            Spacer(modifier = Modifier.height(7.5.dp))
+                                        Modifier
+                                            .width(160.dp)
+                                            .height(50.dp),
+                                        textStyle = TextStyle(fontSize = 12.sp),
+                                        keyboardOptions = KeyboardOptions(
+                                            imeAction = ImeAction.Done,
+                                            keyboardType = KeyboardType.Number
+                                        ),
+                                        keyboardActions = KeyboardActions(
+                                            onDone = { controller?.hide() }
+                                        ),
+                                        label = { Text("Latitude") },
+                                        singleLine = true
+                                    )
 
+                                }
+                                Spacer(modifier = Modifier.height(5.dp))
+                                Row {
+                                    OutlinedButton(modifier = Modifier.width(155.dp), onClick = {
+                                        homeScreenViewModel.getForecastByCord(lat, lon)
+                                        mapViewportState.flyTo(
+                                            cameraOptions = cameraOptions {
+                                                center(Point.fromLngLat(lon, lat))
 
-
-
+                                            },
+                                        )
+                                        scope.launch {
+                                            scaffoldState.bottomSheetState.expand()
                                         }
-                                        else{
-                                            return@breaking
+                                    }) {
+                                        Text("Legg til favoritter")
+
+                                    }
+                                    Spacer(modifier = Modifier.width(25.dp))
+                                    Button(modifier = Modifier.width(155.dp), onClick = {
+                                        homeScreenViewModel.getForecastByCord(lat, lon)
+                                        mapViewportState.flyTo(
+                                            cameraOptions = cameraOptions {
+                                                center(Point.fromLngLat(lon, lat))
+
+                                            },
+                                        )
+                                        scope.launch {
+                                            scaffoldState.bottomSheetState.expand()
                                         }
+                                    }) {
+                                        Text("Hent værdata")
 
                                     }
 
                                 }
 
-                            } })
+                                Spacer(modifier = Modifier.height(5.dp))
+                                LazyRow(
+                                    modifier = Modifier.width(340.dp),
+
+                                    content = {
+                                        favoritter.forEach {
+                                            item {
+                                                ElevatedCard(modifier = Modifier.height(50.dp)) {
+                                                    Text(it)
+
+                                                }
+                                            }
+                                        }
+                                    })
+                                LazyColumn(content = {
+                                    item {
+                                        forecast.foreCast.forEach breaking@{ input ->
+                                            input.properties.timeseries.forEach lit@{ tider ->
+                                                if (tider.time < formattedInstant) {
+                                                    return@lit
+                                                }
+                                                if (formattedInstant < tider.time && tider.time < formattedInstantAfter) {
+                                                    val klokkeslett = tider.time.substring(11, 16)
+                                                    Spacer(modifier = Modifier.height(7.5.dp))
+                                                    ElevatedCard(
+                                                        modifier = Modifier
+                                                            .height(100.dp)
+                                                            .width(340.dp),
+                                                        onClick = {
+                                                            val json =
+                                                                Uri.encode(Gson().toJson(tider.data.instant.details))
+                                                            navController.navigate("DetailsScreen/${json}")
+                                                        })
+                                                    {
 
 
+                                                        Text("Kl:$klokkeslett")
+                                                        Text(" ${tider.data.instant.details.airTemperature}°C")
+                                                        tider.data.next1Hours?.summary?.let {
+                                                            Text(
+                                                                it.symbolCode
+                                                            )
+                                                        }
 
 
+                                                    }
 
-                })
-
-                }
-
+                                                    Spacer(modifier = Modifier.height(7.5.dp))
 
 
-            } ) {
+                                                } else {
+                                                    return@breaking
+                                                }
+
+                                            }
+
+                                        }
+
+                                    }
+                                })
+
+
+                            })
+
+                    }
+
+
+                }) {
 
                 MapboxMap(
                     modifier = Modifier.fillMaxSize(),
@@ -330,14 +400,15 @@ fun HomeScreen(
                             homeScreenViewModel.lat.value = it.latitude()
                             homeScreenViewModel.lon.value = it.longitude()
                             val annotationApi = mapView.annotations
-                            val pointAnnotationManager = annotationApi.createPointAnnotationManager()
-                            val pointAnnotationOptions: PointAnnotationOptions = PointAnnotationOptions()
-                                .withPoint(Point.fromLngLat(lon, lat))
-                                .withIconImage("https://docs.mapbox.com/android/maps/examples/red_marker.png")
+                            val pointAnnotationManager =
+                                annotationApi.createPointAnnotationManager()
+                            val pointAnnotationOptions: PointAnnotationOptions =
+                                PointAnnotationOptions()
+                                    .withPoint(Point.fromLngLat(lon, lat))
+                                    .withIconImage("https://docs.mapbox.com/android/maps/examples/red_marker.png")
                             pointAnnotationManager.create(pointAnnotationOptions)
                             false
                         }
-
 
 
                     }
@@ -348,7 +419,6 @@ fun HomeScreen(
         }
 
     }
-
 
 
 }
